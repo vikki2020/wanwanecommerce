f62f8abfa3882a6ba93190124a1cd642011b2ad6
shin yuh wang <7535835-vikki2021@users.noreply.gitlab.com>
2021-04-16 17:21:22 +0200

    mapping product with easy admin and add products with nice pics( not using fixures at this moment)

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -33,7 +33,10 @@ class Category
     {
         $this->products = new ArrayCollection();
     }
-
+    public function __toString()
+    {
+        return $this->getName();
+    }
     public function getId(): ?int
     {
         return $this->id;
